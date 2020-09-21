5842fafa038d8edc43e71e540e0ee7fdd2c88538
Danial <d.senejohnny@gmail.com>
2020-09-21 17:57:03 +0200

    decorators.py got linted

---
+++ preprocessing/decorators.py
@@ -0,0 +1,60 @@
+"""Decorators used"""
+import time
+import logging
+import functools
+
+
+# timer decorator
+def timer(func):
+    """Find the processing time of a function"""
+
+    @functools.wraps(func)
+    # Without the use of functools.wraps(), the metadata (function name,
+    # docstring, ...) would have been changed to the 'wrapper'
+    def wrapper(*args, **kwargs):
+        tic = time.perf_counter()
+        result = func(*args, **kwargs)
+        diff = time.perf_counter() - tic
+        print(f"Function {func.__name__} executed in {diff} seconds")
+        return result
+
+    return wrapper
+
+
+# logger decorator
+def logger(func):
+    """Log the function signature and return value"""
+    logging.basicConfig(
+        filename=f"./logging/{func.__name__}.log", level=logging.INFO,
+    )
+
+    @functools.wraps(func)
+    def wrapper_logger(*args, **kwargs):
+        args_repr = [repr(a) for a in args]  # 1
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
+        signature = ", ".join(args_repr + kwargs_repr)  # 3
+        value = func(*args, **kwargs)
+        logging.info(
+            f"""Calling {func.__name__!r} with input: {signature}
+            \n Resulted output: {value!r}"""
+        )
+        return value
+
+    return wrapper_logger
+
+
+# debugger decorator
+def debug(func):
+    """Print the function signature and return value"""
+
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        args_repr = [repr(a) for a in args]  # 1
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
+        signature = ", ".join(args_repr + kwargs_repr)  # 3
+        print(f"Calling {func.__name__}({signature})")
+        result = func(*args, **kwargs)
+        print(f"{func.__name__!r} returned {result!r}")  # 4
+        return result
+
+    return wrapper_debug
